10ec38777c62e7e2eac55db86bb132bffb934ccc
realzzz <real_z@yeah.net>
2021-10-27 23:12:52 +0800

    feat: add email

--- index.js
+++ index.js
@@ -1,9 +1,39 @@
 const Cron = require("cron").CronJob;
 const axios = require('axios');
-const { getConfig } = require("./util.js");
+const fs = require("fs");
+const configPath = "./config.json";
+const nodeMailer = require("nodemailer");
 
-// 获取今天免费抽奖的次数
-const getTodayDrawStatus = async () => {
+const getConfig = () => {
+  return JSON.parse(fs.readFileSync(configPath));
+};
+
+const sendEmail = async (subject, html) => {
+  let cfg = getConfig().email.yeah;
+  if (!cfg || !cfg.user || !cfg.pass) return;
+  const transporter = nodeMailer.createTransport({
+    host: "smtp.yeah.net",
+    port: 25,
+    secureConnection: true,
+    auth: { user: cfg.user, pass: cfg.pass },
+  });
+  transporter.sendMail(
+    {
+      from: cfg.from,
+      to: cfg.to,
+      subject: subject,
+      html: html,
+    },
+    (err) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+    }
+  );
+};
+
+const getTodayLotteryStatus = async () => {
   const { cookie, baseUrl, apiUrl } = getConfig();
   let { data } = await axios({
     url: baseUrl + apiUrl.getLotteryConfig,
@@ -15,21 +45,23 @@ const getTodayDrawStatus = async () => {
   if (data.err_no) {
     return {
       error: true,
-      isDraw: false,
+      isLotteryed: false,
     };
   } else {
     return {
       error: false,
-      isDraw: data.data.free_count === 0,
+      isLotteryed: data.data.free_count === 0,
     };
   }
 };
 
 // 抽奖
 const draw = async () => {
-  let { error, isDraw } = await getTodayDrawStatus();
-  if (error) return;
-  if (isDraw) return;
+  let { error, isLotteryed } = await getTodayLotteryStatus();
+  if (error || isLotteryed) {
+    await sendEmail('抽过啦', 'QAQ');
+    return;
+  } 
   const { cookie, baseUrl, apiUrl } = getConfig();
   let { data } = await axios({
     url: baseUrl + apiUrl.drawLottery,
@@ -38,7 +70,12 @@ const draw = async () => {
       Cookie: cookie,
     },
   });
-  if (data.err_no) return;
+  if (data.err_no) {
+    await sendEmail('抽奖失败啦', 'QAQ');
+    return;
+  }
+  await sendEmail('抽奖结果', data);
+
 };
 
 // 查询今日是否已经签到
@@ -51,7 +88,6 @@ const getTodayCheckStatus = async () => {
       Cookie: cookie,
     },
   });
-  console.log(data);
   return {
     error: data.err_no !== 0,
     isCheck: data.data,
@@ -61,8 +97,14 @@ const getTodayCheckStatus = async () => {
 // 签到
 const checkIn = async () => {
   let { error, isCheck } = await getTodayCheckStatus();
-  if (error) return;
-  if (isCheck) return;
+  if (error) {
+    await sendEmail('验证签到错误', 'QAQ');
+    return;
+  }
+  if (isCheck) {
+    await sendEmail('已经签到', 'QAQ');
+    return;
+  }
   const { cookie, baseUrl, apiUrl } = getConfig();
   let { data } = await axios({
     url: baseUrl + apiUrl.checkIn,
@@ -72,14 +114,14 @@ const checkIn = async () => {
     },
   });
   if (data.err_no) {
-    console.log('fail')
+    await sendEmail('签到失败', data);
   } else {
-    console.log('success')
+    await sendEmail('签到成功', 'OVO');
   }
 };
 
 let job = new Cron(
-  "0 23 0 * * *",
+  "10 10 10 * * *",
   async () => {
     await checkIn();
     await draw();
